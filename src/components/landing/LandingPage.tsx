2badf22b631da854ea7ddc2e83165022f855be59
Michiel Hebly <michiel.hebly@hotmail.com>
2025-12-15 20:41:37 +0100

    Fix logo and favicon assets

--- src/components/landing/LandingPage.tsx
+++ src/components/landing/LandingPage.tsx
@@ -16,7 +16,7 @@ export default function LandingPage() {
                 <View style={styles.header}>
                     <View style={styles.logoContainer}>
                         <Image
-                            source={require('../../../assets/icon.png')}
+                            source={require('../../../assets/krib-logo-small.png')}
                             style={styles.logo}
                             resizeMode="contain"
                         />
